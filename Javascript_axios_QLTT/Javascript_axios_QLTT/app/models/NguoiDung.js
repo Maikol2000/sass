e117f6456c372e7f7e0a6ace54d03bb9b734620b
Maikol2000 <cuonglw27@gmail.com>
2021-07-12 15:04:43 +0700

    new

---
+++ Javascript_axios_QLTT/Javascript_axios_QLTT/app/models/NguoiDung.js
@@ -0,0 +1,9 @@
+function NguoiDung(taiKhoan, hoTen, matKhau, email, hinhAnh, loaiNN, moTa) {
+    this.taiKhoan = taiKhoan;
+    this.hoTen = hoTen;
+    this.matKhau = matKhau;
+    this.email = email;
+    this.hinhAnh = hinhAnh;
+    this.loaiNN = loaiNN;
+    this.moTa = moTa;
+}
